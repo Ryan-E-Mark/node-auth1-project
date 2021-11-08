9649abde76c635fb97e09c04a47f09393d5b99da
Ryan Mark <rymark907@gmail.com>
2021-11-08 12:15:26 -0800

    set up server.js to use cookies and create a session for logins

--- api/server.js
+++ api/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 
+const session = require('express-session')
+const Store = require('connect-session-knex')(session)
 /**
   Do what needs to be done to support sessions with the `express-session` package!
   To respect users' privacy, do NOT send them a cookie unless they log in.
@@ -20,6 +22,24 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use(session({
+  name: 'chocolatechip',
+  cookie: {
+    maxAge: 1000 * 60,
+    secure: false,
+    httpOnly: false,
+  },
+  resave: false,
+  saveUninitialized: false,
+  store: new Store({
+    knex: require('../data/db-config'),
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+    createtable: true,
+    clearInterval: 1000 * 60 * 60,
+  })
+}))
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
